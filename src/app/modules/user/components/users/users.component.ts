8ace0f066ede4798ad3cabb0184eddac86e761b2
vmakar9 <zmerin212@gmail.com>
2022-11-01 13:34:59 +0200

    1

--- src/app/modules/user/components/users/users.component.ts
+++ src/app/modules/user/components/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../../services/user.service";
 import {IUser} from "../../interfaces/user.interface";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-users',
@@ -11,10 +12,10 @@ export class UsersComponent implements OnInit {
 
   users:IUser[]
 
-  constructor(private userService:UserService) { }
+  constructor(private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.userService.getAll().subscribe(value => this.users=value)
+    this.activatedRoute.data.subscribe(({users})=>this.users=users)
   }
 
 }
--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {UsersComponent} from "./components/users/users.component";
 import {UserDetailsComponent} from "./components/user-details/user-details.component";
+import {UserResolver} from "./services/resolver/user.resolver";
 
 const routes: Routes = [
-  {path:'',component:UsersComponent,children:[
+  {path:'',component:UsersComponent,resolve:{users:UserResolver},children:[
       {path:':id',component:UserDetailsComponent}
     ]}
 ];
