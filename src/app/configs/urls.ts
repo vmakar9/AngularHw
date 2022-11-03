42710b0aa3b43d2341372317796a81e918db0070
vmakar9 <zmerin212@gmail.com>
2022-11-03 22:31:09 +0200

    1

---
+++ src/app/configs/urls.ts
@@ -0,0 +1,15 @@
+import {environment} from "../../environments/environment";
+
+const {API} = environment
+
+const auth = `${API}/auth`
+
+const urls={
+  cars:`${API}/cars`,
+  auth:{
+    login:auth,
+    refresh:`${auth}/refresh`
+  }
+}
+
+export {urls}
